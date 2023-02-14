4f7b732649cf4a7874c5e56fa2b5a0f54f67b5a7
lucas Souza domingues <elesouzadom@gmail.com>
2023-02-13 22:01:22 -0400

    first commit

---
+++ todo_app/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import './view/pages/login_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Todo App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      routes: {
+        '/': (context) => Login(),
+      },
+    );
+  }
+}
