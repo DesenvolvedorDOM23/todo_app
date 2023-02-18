33cc2b632f71bb1184f116d34529df9a8a7fab40
lucas Souza domingues <elesouzadom@gmail.com>
2023-02-18 19:39:34 -0400

    inicio tela logi_page

--- todo_app/lib/main.dart
+++ todo_app/lib/main.dart
@@ -12,7 +12,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Todo App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
